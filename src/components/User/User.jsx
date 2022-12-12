6b63fec0263439145e94ce4bc66983da6734c3e7
jigooo <h00kk@naver.com>
2022-12-12 23:22:14 +0900

    1212

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,7 +4,7 @@ import "../User/User.scss";
 const User = ({ user: { displayName, photoURL } }) => {
   return (
     <div className="user-image">
-      <img src={photoURL} alt={displayName} />
+      <img src={photoURL} alt={displayName} referrerPolicy="no-referrer" />
       <span>{displayName}</span>
     </div>
   );
